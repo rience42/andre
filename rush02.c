2a24a0bab6f7880d65ed1fbdfff24ef302f6eb88
rience42 <161425408+rience42@users.noreply.github.com>
2024-03-03 00:10:36 +0100

    Update rush02.c

--- rush02.c
+++ rush02.c
@@ -10,5 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ft_putchar.c> 
+#include <unistd.h>
 
+void	ft_putchar(char b);
+
+// Funcao para imprimir os cantos superiores (A e C)
+void	cantos_superiores(int largura)
+{
+	int	j;
+
+	ft_putchar('A');
+	j = 1;
+	while (j < largura - 1)
+	{
+		ft_putchar('B');
+		j++;
+	}
+	ft_putchar('C');
+	ft_putchar('\n');
+}
+
+// Funcao para imprimir os cantos laterais (B)
+void	cantos_laterais(int compri)
+{
+	int	i;
+
+	ft_putchar('B');
+	i = 1;
+	while (i < compri - 1)
+	{
+		ft_putchar(' ');
+		i++;
+	}
+	ft_putchar('B');
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	cont;
+
+	cont = 0;
+	while (cont < y)
+	{
+		if (cont == 0 || cont == y - 1)
+		{
+			cantos_superiores(x);
+		}
+		else
+		{
+			cantos_laterais(x);
+		}
+		cont++;
+	}
+}
